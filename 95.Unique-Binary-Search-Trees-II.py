4b2551c46a6c1250f28b9a602a157031b36006db
Robertren <zizhuoren@gmail.com>
2017-01-20 11:29:37 -0500

    Tree
    Tree problem with BST

---
+++ 95.Unique-Binary-Search-Trees-II.py
@@ -0,0 +1,29 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        this.val = val
+        this.left, this.right = None, None
+"""
+class Solution:
+    # @paramn n: An integer
+    # @return: A list of root
+    def generateTrees(self, n):
+        # write your code here
+        if n == 0:
+            return []
+        return self.dfs(1, n)
+        
+    def dfs(self, start, end):
+        if start > end: return [None]
+        res = []
+        for rootval in range(start, end+1):
+            LeftTree = self.dfs(start, rootval-1)
+            RightTree = self.dfs(rootval+1, end)
+            for i in LeftTree:
+                for j in RightTree:
+                    root = TreeNode(rootval)
+                    root.left = i
+                    root.right = j
+                    res.append(root)
+        return res
